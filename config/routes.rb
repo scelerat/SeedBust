62a038fe59d53ce3f7473486be36c2ca965e583d
Finn Kelly <finn@thegogame.com>
2010-05-29 18:33:16 -0700

    types adding and editing

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :types
+
+  map.resources :types
+
+  map.resources :species
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
